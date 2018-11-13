2382cbb5e1c3bcaca821c3cf64587bd0e5bd6d58
Eric Ampire <ericampire.top@gmail.com>
2018-11-13 22:55:19 +0200

    Mise en place du splash

--- app/src/main/java/com/pbreakers/webview/SplashActivity.kt
+++ app/src/main/java/com/pbreakers/webview/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.pbreakers.webview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,14 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        object : Thread() {
+            override fun run() {
+                Thread.sleep(1500)
+
+                startActivity(Intent(baseContext, MainActivity::class.java))
+                finish()
+            }
+        }.start()
     }
 }
